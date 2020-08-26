c161cae27907c8814a8e2b81759ac4dd81162fe6
Sheikh Sadaf Tasin <tasins@uwindsor.ca>
2020-08-26 05:35:08 -0400

    Initial commit

---
+++ assets/js/todos.js
@@ -0,0 +1,29 @@
+//Check off done todos by clicking
+$("ul").on("click", "li", function(){
+   $(this).toggleClass("completed");
+});
+
+
+//Click on X to delete a Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//Input Todos
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //inserting user input in a variable
+        var todoInputText = $(this).val();
+        //empties the text box
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append(`<li><span><i class='far fa-trash-alt'></i></span> ${todoInputText} </li>`)
+    }
+
+});
+
+
